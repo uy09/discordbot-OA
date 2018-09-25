24591cc1b65784f1995a8de5154380fcc464c73f
uy09 <43575719+uy09@users.noreply.github.com>
2018-09-25 15:59:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,24 @@
-const Discord = require ('discord.io');
-const client = new Discord.Client();
+const Discord = require("discord.io");
+const bot = new Discord.Client({disableEveryone: true});
 
-client.on('ready', () => {
-    console.log('I am ready!');
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`Hey, Wassup!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-      message.reply('pong');
-    }
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '?';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}ping`){
+    message.channel.send("Pong!");
+  }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.token);
